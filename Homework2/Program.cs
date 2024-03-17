98f188da8d6ec6609912a38623c022dfb473c956
Viktorya <Viktorya@DESKTOP-AHJPH50>
2024-03-18 03:56:46 +0400

    Add project files.

---
+++ Homework2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace problem1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> myList = new List<int> { 10, 4, 9, 1, 5 };
+
+            List<int> middleElements = FindMiddleElements(myList);
+
+            Console.Write("Middle Element: ");
+            foreach (int element in middleElements)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static List<int> FindMiddleElements(List<int> list)
+        {
+            List<int> middleElements = new List<int>();
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("The list is empty.");
+            }
+            else
+            {
+                int middleIndex = list.Count / 2;
+                middleElements.Add(list[middleIndex]);
+            }
+
+            return middleElements;
+        }
+    }
+}
---
+++ problem2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> myList = new List<int> { 1, 7, 3, 4, 5 };
+
+            int biggestNumber = FindBiggestNumber(myList);
+
+            Console.WriteLine("Biggest Number: " + biggestNumber);
+        }
+
+        static int FindBiggestNumber(List<int> list)
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("No numbers to compare.");
+                return -1;
+            }
+
+            int biggest = list[0];
+
+            foreach (int number in list)
+            {
+                if (number > biggest)
+                {
+                    biggest = number;
+                }
+            }
+
+            return biggest;
+        }
+    }
+}
